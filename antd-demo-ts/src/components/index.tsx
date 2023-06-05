89b1b67baf52d65a091e04aa9d5faa1f6a51863c
zeeshanshaheenn3o <112391016+zeeshanshaheenn3o@users.noreply.github.com>
2023-06-05 18:43:34 +0500

    Create index.tsx

---
+++ antd-demo-ts/src/components/index.tsx
@@ -0,0 +1,8 @@
+// you can create you component in this folder and export them in index file.
+
+
+export {};
+
+
+// you can export your components by importing them at the top and add your components in 
+// export {A,B,C};
