2559aab5b33a2f709f662ae444142c21df7bebf5
ZeeshanShaheen <zeeshan.shaheen@n3o.ltd>
2023-06-05 18:31:29 +0500

    Setting Up the Project

---
+++ antd-demo-ts/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the Task.
+          Try not to over engineer the solution.
+          After Finishing Upload it to Github and send us the Repo Link.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
